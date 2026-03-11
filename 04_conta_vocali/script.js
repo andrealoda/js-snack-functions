da2e9a9e6b0dd38ab321c944a0247c6c653d0241
andrealoda <andrea@casaloda.it>
2026-03-11 14:56:23 +0100

    complete 04_conta_vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,39 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+/* Scrivi una funzione che accetti una stringa e 
+restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function vowelsReturn(startWord) {
+    const vowels = 'aeiouy';
+    let result = 0;
+    const foundVowel = [];
+
+    for (let i = 0; i < word.length; i++) {
+        if (vowels.includes(word[i])) {
+            result++;
+            foundVowel.push(word[i]);
+        }
+    }
+
+    return result, foundVowel;
+}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(vowelsReturn(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+
+
+
+
+
+
+
+
+
+
