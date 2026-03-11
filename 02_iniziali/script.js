04a7687a625b2c3a66d3fe6f3eea7e0a43e610c4
andrealoda <andrea@casaloda.it>
2026-03-11 10:14:27 +0100

    complete 02_iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,27 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e 
+restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function arrFinale(arrOrigine) {
 
+    const arrIniziali = [];
+    
+    for (let i = 0; i < arrOrigine.length; i++) {
+        const element = arrOrigine[i];
+        arrIniziali.push(element[0]);
+    }
+
+    return arrIniziali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(arrFinale(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+
+
