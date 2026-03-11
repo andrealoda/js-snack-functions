4b3f4c01eccc0ed5273bea9bd46cf515daae7068
andrealoda <andrea@casaloda.it>
2026-03-11 11:56:59 +0100

    delete unuseful comment in 03_filtra_iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -26,25 +26,3 @@ return arrFinal
 console.log(arrStartsWith(initialLetter, names));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
-
-
-
-/*
-se il primo elemento di ogni elemento dell'array
-è uguale
-alla lettera scelta
-allora
-salva l'elemento dell'array
-in un nuovo array
-
-for (let i = 0; i < arrOrigin.length; i++) {
-const element = arrOrigin[i][0]
-const arrFinal = [];
-
-    if (initialLetter === element {
-    arrFinal.push(element)
-    }
-
-}
-
-*/
