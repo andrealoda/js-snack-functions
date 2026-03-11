c288eae5830e1e33b4a344f240f22a87aeb375dd
andrealoda <andrea@casaloda.it>
2026-03-11 09:30:58 +0100

    complete 01_saluti

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,22 @@
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+/* Scrivi una funzione che accetti una stringa contenente un nome e 
+restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
+function saluto(yourName) {
+    return 'Ciao ' + yourName
+}
 
+
+// arrow function
+// const saluto = yourName => 'Ciao ' + yourName;
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+saluto(userName);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+
+console.log(saluto(userName));
